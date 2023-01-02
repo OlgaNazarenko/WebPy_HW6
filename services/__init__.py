42912f500ce231e3dfc131bd4a2d5165814b7705
olganazarenko <113229061+olganazarenko@users.noreply.github.com>
2023-01-02 06:55:59 +0200

    Update __init__.py

--- services/__init__.py
+++ services/__init__.py
@@ -1,3 +1,4 @@
 from services.create_table import create_table
 from services.select_tables import select_tables
 from services.fill_table import fill_table
+from services.settings import SQL_QUERY, DATABASE
